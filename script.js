44e2428fcf5cf99f646d81dc33a55150698954ea
seokh1213 <seokh1213@gmail.com>
2020-04-22 13:51:15 +0900

    http -> https

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log("123");
 
-fetch("http://ddragon.leagueoflegends.com/cdn/10.8.1/data/ko_KR/champion.json")
+fetch("https://ddragon.leagueoflegends.com/cdn/10.8.1/data/ko_KR/champion.json")
   .then((res) => res.json())
   .then((res) => console.log(res.data));
