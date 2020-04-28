b6c390315e6a6256fc0df087d487eff5d3301eac
seokh1213 <seokh1213@gmail.com>
2020-04-28 22:42:48 +0900

    Updates

---
+++ precache-manifest.bb1c7d168cec2cc665e593bc889eaa11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c11242fef4ca3e4802647bb7706b18b4",
+    "url": "/cooldown/index.html"
+  },
+  {
+    "revision": "070c628d1b31492a8412",
+    "url": "/cooldown/static/js/2.421ddbf1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cooldown/static/js/2.421ddbf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d994650913939ebd5ac1",
+    "url": "/cooldown/static/js/main.e5e34787.chunk.js"
+  },
+  {
+    "revision": "d120c84360ba07fc15ad",
+    "url": "/cooldown/static/js/runtime-main.89f98d3d.js"
+  }
+]);
