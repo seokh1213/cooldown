251a13f13281618ff5a7ef8ff0318b6a84874091
seokh1213 <seokh1213@users.noreply.github.com>
2026-09-22 03:21:01 +0000

    deploy: ef5a1cbc25b52c76264f1709eb7963bbfd0aece1

--- pwa-release-14d397c76157cbc3ae453a9ae7f99421.js
+++ pwa-release-14d397c76157cbc3ae453a9ae7f99421.js
@@ -1,5 +1,5 @@
 
-const release = {"schemaVersion":1,"appVersion":"a39eebec1bbed3291834bbf2b83e6e88","dataVersion":"6321d44a0ff25fc4ce49770b6b47f864","releaseId":"6661ec2112ca8c358251b08711480bd4","patchVersion":"26.18"};
+const release = {"schemaVersion":1,"appVersion":"4156aa5f55b7e48e3aa876b4722c9e0d","dataVersion":"6321d44a0ff25fc4ce49770b6b47f864","releaseId":"14d397c76157cbc3ae453a9ae7f99421","patchVersion":"26.18"};
 self.addEventListener('message', (event) => {
   if (event.data?.type === 'COOLDOWN_RELEASE') event.ports[0]?.postMessage(release);
 });
